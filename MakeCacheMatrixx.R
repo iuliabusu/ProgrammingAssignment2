0742498a2a5239d50aac3a853fd8b0c6f58a9f2e
Andreea Iulia <andreea_dj_deea@yahoo.com>
2020-11-21 16:22:44 +0100

    makeCacheMatrix

---
+++ MakeCacheMatrixx.R
@@ -0,0 +1,85 @@
+makeCacheMatrix <- function(x = matrix()) {
+     ## Initiate inverse to NULL matrix
+            inverse <- NULL
+                                                                
+     ## Reset matrix 'x' to new value from set() function and reset inverse to NULL
+         set <- function(y) {
+              x <<- y
+            inverse <<- NULL
+ }
+                                                                
+     ## returns matrix
+             get <- function() x
+                                                                
+     ## Once inverse is calculated, set the inverse attribute of the vector to the value
+              setInverse <- function(i) { 
+              inverse <<- i
+}
+
+     ## return the value of the inverse
+               getInverse <- function() inverse
+                                                                
+     ## List methods inside makeCacheMatrix
+                list(set = set, get = get,
+                setInverse = setInverse,
+             getInverse = getInverse)
+}
+##
+##    cacheSolve() will compute the inverse of the special matrix returned by makeCacheMatrix(). If the inverse has 
+##    already been calculated and the matrix has not changed, then cacheSolve() will return the inverse from cache.
+##
+cacheSolve <- function(x, ...) {
+       ## Return a matrix that is the inverse of 'x'
+       ## Once you've made a vector 'x' with some "special properties" 
+       ## you can call on these special properties, like getInverse(), etc...
+                inv <- x$getInverse()
+                                                                
+         ## if the inverse is not null then return the inverse
+                 if(!is.null(inv)) {
+                message("getting cached data")
+                 return(inv)
+}
+                                                                
+        ## else, if the inverse has not been stored in getInverse() yet then 
+        ## get the matrix you stored in the vector and call it data
+                  data <- x$get()
+                                                                
+        ## now calculate the inverse of the matrix
+                 inv <- solve(data)
+                                                                
+        ## store the mean in the special vector using the method setmean()
+                   x$setInverse(inv)
+                                                                
+         ## Print Inverse Matrix!
+                  inv
+}
+
+##    Unit test cases
+
+##Choose an invertible matrix called 'a'
+a <- rbind(c(3,4),c(3,3))
+b <- rbind(c(1,2,3),c(0,2,4), c(5,7,0))
+## Check its invertible. YES!
+solve(a)
+solve(b)
+## Make a matrix with special properties
+x <- makeCacheMatrix(a)
+
+##    Check get()
+x$get()
+
+##    Check set()
+x$set(b)
+x$get()
+
+##    Check getInverse()
+x$getInverse()
+
+##    Check setInverse()
+x$setInverse( solve(a) )
+x$getInverse()
+
+##    Check cacheSolve()
+## Calculate Inverse Matrix and return it
+cacheSolve(x)
+Fin
